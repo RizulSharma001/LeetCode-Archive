b99b77c67c3ed6298ffb224299f7cce40c1540ed
Rizul Sharma <147021709+RizulSharma001@users.noreply.github.com>
2025-12-31 12:00:00 +0530

    Time: 0 ms (100.00%) | Memory: 43.5 MB (21.41%) - LeetSync

---
+++ 1505-create-target-array-in-the-given-order/create-target-array-in-the-given-order.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int[] createTargetArray(int[] nums, int[] index) {
+        List<Integer> targetList = new ArrayList<>();
+        for(int i=0;i<nums.length;i++){
+            targetList.add(index[i],nums[i]);
+        }
+        int[] targetArray = new int[nums.length];
+        for(int i=0;i<targetList.size();i++){
+            targetArray[i]=targetList.get(i);
+        }
+        return targetArray;
+    }
+}
