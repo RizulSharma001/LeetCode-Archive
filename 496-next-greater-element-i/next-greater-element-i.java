69560819ed42bc6c57e96a2e05f0508fcd1ac820
Rizul Sharma <147021709+RizulSharma001@users.noreply.github.com>
2026-01-27 03:47:31 +0530

    Time: 4 ms (73.50%) | Memory: 45.6 MB (8.42%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.java
+++ 496-next-greater-element-i/next-greater-element-i.java
@@ -1,28 +1,40 @@
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        Stack<Integer> st = new Stack<>();
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for(int i=0;i<nums2.length;i++){
+            int x = nums2[i];
+            while(!st.isEmpty() && x > st.peek()){
+                int smaller = st.pop();
+                map.put(smaller,x);
 
-        int[] res = new int[nums1.length];
-
-        boolean found = false;
-        
-        for(int i=0;i<nums1.length;i++){
-            found = false;
-            for(int j=0;j<nums2.length;j++){
-                if(nums1[i]==nums2[j]){
-                    found = true;
-                }
-                if(found && nums2[j] > nums1[i]){
-                    res[i] = nums2[j];
-                    break;
-                }
             }
+            st.push(x);
         }
-        for(int i=0;i<res.length;i++){
-            if(res[i] == 0){
-                res[i] = -1;
-            }
+        while(!st.isEmpty()){
+            map.put(st.pop(), -1);
+        }
+        int[] res = new int[nums1.length];
+        for(int i=0;i<nums1.length;i++){
+            res[i] = map.get(nums1[i]);
         }
         return res;
-        
+
     }
 }
+
+
+/* Create empty stack
+Create empty hashmap
+
+for each element x in nums2:
+    while stack not empty and x > stack.top:
+        map[stack.pop()] = x
+    push x into stack
+
+while stack not empty:
+    map[stack.pop()] = -1
+
+for each element y in nums1:
+    answer[i] = map[y]
+ */
